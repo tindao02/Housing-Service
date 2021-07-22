b7d7677a43e2cfdde633020e37e41a155c7861eb
tindao02 <timdao02@hotmail.com>
2021-07-22 20:10:36 -0300

    Criação do projeto

---
+++ src/test/java/com/housing/HousingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
